d3f7110a657b99fa650481b0906b63c37235ec8c
Adam <adam-26@users.noreply.github.com>
2017-12-07 00:36:39 +0100

    fix(bug): factory method
    Correctly parse parameters for dispatching actions on the server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import invariant from 'invariant';
 import { withRouter } from 'react-router';
+import { parsePath } from 'history'
 import hoistNonReactStatic from 'hoist-non-react-statics';
-import { RouteDispatcher, RouteDispatcherPropTypes } from './RouteDispatcher';
+import { RouteDispatcher, RouteDispatcherPropTypes, standardizeDispatchActions, DEFAULT_DISPATCH_ACTIONS } from './RouteDispatcher';
 import defineRoutes from './defineRoutes';
 
 const RouterDispatcher = withRouter(RouteDispatcher);
@@ -28,9 +30,16 @@ function RouteDispatcherHoc(routeConfig, options) {
 }
 
 // use a factory method to simplify server usage
-function createRouteDispatcher(location, routeConfig, options) {
+function createRouteDispatcher(pathAnyQuery, routeConfig, options = {}) {
+    invariant(typeof pathAnyQuery === 'string', 'pathAnyQuery expects a string');
+    invariant(Array.isArray(routeConfig), 'routeConfig expects an array of routes');
+
     return {
-        dispatchOnServer: () => RouterDispatcher.dispatchActions(location, { ...options, routes: routeConfig }),
+        dispatchOnServer: () =>
+            RouterDispatcher.dispatch(
+                parsePath(pathAnyQuery),
+                standardizeDispatchActions(options.dispatchActions || DEFAULT_DISPATCH_ACTIONS),
+                { ...options, routes: routeConfig }),
         RouteDispatcher: RouteDispatcherHoc(routeConfig, { ...options, hasDispatchedActions: true }),
     };
 }
